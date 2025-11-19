7f07c3d18e410bfcc4ab40728e5663e263ec1271
Mohammed <mohayder9199@gmail.com>
2025-11-19 19:27:11 +0300

    Add project files.

---
+++ Data/PgDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using VideoGameCharacterApi.Models; // adjust namespace
+
+namespace VideoGameCharacterApi.Data
+{
+    public class PgDbContext : DbContext
+    {
+        public PgDbContext(DbContextOptions<PgDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Database tables
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Product> Products { get; set; }
+    }
+}
